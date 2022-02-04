b73e466471167ff9edfcd017dbf4b7063d4424dc
jartificer <radu.s.mailat@gmail.com>
2022-02-04 12:21:16 +0200

    implement model mapper for save and findByIed

--- src/main/java/com/example/liquorstore/service/LiquorService.java
+++ src/main/java/com/example/liquorstore/service/LiquorService.java
@@ -3,9 +3,8 @@ package com.example.liquorstore.service;
 import com.example.liquorstore.model.LiquorDto;
 import com.example.liquorstore.repository.liquors.Liquor;
 import com.example.liquorstore.repository.liquors.LiquorRepository;
-import liquibase.pro.packaged.id;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.Optional;
 
@@ -15,7 +14,7 @@ public class LiquorService {
 
   //  public List<LiquorDto> getAllLiquors(){
   //    return liquorRepository.findAll();
-  // de folosit --object-- (de fapt model) mapper
+  // de folosit model mapper
   //  }
 
   public LiquorService(LiquorRepository liquorRepository) {
@@ -23,20 +22,15 @@ public class LiquorService {
   }
 
   public LiquorDto save(LiquorDto liquorDto) {
-    Liquor newLiquor = new Liquor();
-    newLiquor.setName(liquorDto.getName());
-    newLiquor.setId(liquorDto.getId());
-    newLiquor.setProducer(liquorDto.getProducer());
-    newLiquor.setAbv(liquorDto.getAbv());
-    newLiquor.setStock(liquorDto.getStock());
+    ModelMapper modelMapper = new ModelMapper();
+    Liquor newLiquor = modelMapper.map(liquorDto, Liquor.class);
     liquorRepository.save(newLiquor);
     return liquorDto;
   }
 
   public LiquorDto findById(int id) {
-    return liquorRepository
-        .findById(id)
-        .map(x -> new LiquorDto(x.getId(), x.getName(), x.getProducer(), x.getAbv(), x.getStock()))
-        .orElseThrow(() -> new RuntimeException("liquor not found"));
+    Optional<Liquor> liquor = liquorRepository.findById(id);
+    ModelMapper modelMapper = new ModelMapper();
+    return modelMapper.map(liquor.orElseThrow(() -> new RuntimeException("liquor not found")), LiquorDto.class);
   }
 }
