e49f3086624fbafb5c670bf0e98693717243a9d9
jartificer <radu.s.mailat@gmail.com>
2022-02-10 11:06:35 +0200

    clean up folder structure

--- src/test/java/com/example/liquorstore/LiquorStoreApplicationTests.java
+++ src/test/java/com/example/liquorstore/LiquorStoreApplicationTests.java
@@ -10,4 +10,5 @@ class LiquorStoreApplicationTests {
 	void contextLoads() {
 	}
 
+
 }
--- src/test/resources/data.sql
+++
@@ -1,1 +0,0 @@
-INSERT INTO Liquor (id, name, producer, abv, stock) values (3, 'liquor3', 'producer1', 5.5, 60)
