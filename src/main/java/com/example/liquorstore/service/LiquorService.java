9760183f3ae8fd867869c67c4237f182be41f97f
jartificer <radu.s.mailat@gmail.com>
2022-02-11 09:56:44 +0200

    clean up

--- src/main/java/com/example/liquorstore/service/LiquorService.java
+++ src/main/java/com/example/liquorstore/service/LiquorService.java
@@ -15,6 +15,7 @@ import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.Optional;
+//test
 
 @Service
 public class LiquorService {
